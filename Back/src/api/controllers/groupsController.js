a708ff97c26538d4586c44785977161dc49fd815
Matthieu B <mesjeux0108@gmail.com>
2022-12-04 19:34:44 +0100

    Api groups

---
+++ Back/src/api/controllers/groupsController.js
@@ -0,0 +1,54 @@
+const Groups = require("../models/groupsModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+exports.CreateGroups = (req, res) =>{
+    let newGroups = new Groups(req.body);
+    newGroups.save((error, groups) => {
+        if(error){
+            res.status(401);
+            console.log(error);
+            res.json({message: "Rêquete invalide"});
+        }
+        else{
+            res.status(200);
+            res.json({message: `Group crée : ${groups.name}`});
+        }
+    });
+}
+
+exports.getGroup = (req,res) =>{
+    Groups.findById(req.params.groupId,(error,group) =>{
+        if(error){
+            res.status(500);
+            console.log(error);
+            res.json({message: "Group non trouvé"});
+        }
+        else{
+            res.status(200);
+            res.json({message: `Group trouvé : ${group}`});
+        }
+    });
+}
+exports.addUser=(req,res) =>{
+    Groups.findById(req.params.groupId,(error,group) =>{
+        if(error){
+            res.status(500);
+            console.log(error);
+            res.json({message: "Group non trouvé"});
+        }
+        else{
+
+            Groups.findByIdAndUpdate({_id:req.params.groupId},{listuser:group.listuser+","+req.body.mail},{new: true},(error,group2)=>{
+                if(error){
+                    res.status(500);
+                    console.log(error);
+                    res.json({message: "Group non trouvé"});
+                }
+                else{
+                    res.status(200);
+                    res.json({message: `Group update : ${group2}`});
+                }
+            })
+        }
+    });    
+}
