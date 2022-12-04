0de92f07f072f24cc53b03a26c738c0968d1889e
Coumbadiankha <coumba.diankha@hotmail.fr>
2022-12-04 22:38:48 +0100

    Update projectModel

---
+++ Back/src/api/models/projectModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let ProjectSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    listgroup:{
+        type: String,
+        required: true,
+    },
+    listuser: {
+        type: String,
+        required: true,
+    },
+    mailadmin: {
+        type: String,
+        required: true,
+    }
+});
+
+module.exports = mongoose.model("Project", ProjectSchema);
