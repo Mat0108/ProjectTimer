e14b2da4ad04869b7cc4318bb62cd7e3318374dd
taniaolivia <taniaolivia@macbook-pro-de-tania.home>
2022-12-04 16:03:01 +0100

    add get all users and user by id routes

--- Back/src/api/routes/userRoute.js
+++ Back/src/api/routes/userRoute.js
@@ -5,4 +5,8 @@ module.exports = (server) => {
 server.post("/user/register", cors(), userController.userRegister);
 server.post("/user/login", cors(), userController.userLogin);
 server.post("/user/logout/:userId", cors(), userController.userLogout);
+
+server.get("/users", cors(), userController.listAllUsers);
+server.get("/user/:userId", cors(), userController.aUser);
+
 }
