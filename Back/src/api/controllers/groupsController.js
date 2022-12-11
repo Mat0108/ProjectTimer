2685532020ac958c76450ac5e94b32f4d3752d0c
Matthieu B <mesjeux0108@gmail.com>
2022-12-11 16:33:26 +0100

    Update groupsController.js

--- Back/src/api/controllers/groupsController.js
+++ Back/src/api/controllers/groupsController.js
@@ -4,76 +4,40 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const axios = require("axios")
 exports.CreateGroups = (req, res) =>{
-    if(req.body.listuser){
-        let listuser = new Array();
-        let listmail = req.body.listuser.split(",")
-        console.log("listmail ",listmail)
-        listmail.map(e=>{
-            console.log("mail:",e)
-            User.findOne({email: e},(error, user) => {
-                console.log("error:",error)
-                if(error){
-                    bcrypt.hash("test1234", 10, (error, hash) => {
-                        if(error){
-                            res.status(401);
-                            console.log(error);
-                            res.json({message: "Impossible de crypter le mot de passe (groups)"});
-                        }
-                        else{
-                            let newUser = new User({firstname:e,lastname:e,email:e,password:hash});
-                            console.log("newUser:",newUser)
-                            newUser.save((error, user) => {
-                                if(error){
-                                    res.status(401);
-                                    console.log(error);
-                                    res.json({message: "Rêquete invalide (groups)"});
-                                }
-                                else{
-                                    res.status(200);                    
-                                    console.log("user:",user)
-                                    listuser.push(user)
-                                }
-                            });
-                        }
-                    })
-                }else{
-                    res.status(200);
-                    
-                    console.log("user:",user)
-                    listuser.push(user)
-                }
-            })
+    let listmail = req.body.listuser.split(",")
+    listmail.map(e=>{                
+        User.findOne({email: e}, (error, user) => {
+            if(error || (!error && !user)){
+                bcrypt.hash("test1234", 10, (error, hash) => {
+                    if(error){
+                        res.status(401);
+                        console.log(error);
+                        res.json({message: "Impossible de crypter le mot de passe (groups)"});
+                    }
+                    else{
+                        let newUser = new User({firstname:e,lastname:e,email:e,password:hash});
+                        newUser.save();
+                    }
+                })
+            }
         })
-        console.log("listuser",listuser)        
-        let newGroups = new Groups({name:req.body.name,listuser:listuser,mailadmin:req.body.mailadmin});
-        newGroups.save((error, groups) => {
-            if(error){
-                res.status(401);
-                console.log(error);
-                res.json({message: "Rêquete invalide"});
-            }
-            else{
-                res.status(200);
-                res.json({message: `Group crée : ${groups.name}`});
-            }
-        });
-    }else{
+    })            
+    let newGroups = new Groups(req.body);
+    newGroups.save((error, groups) => {
+        if(error){
+            res.status(401);
+            console.log(error);
+            res.json({message: "Rêquete invalide"});
+        }
+        else{
+            res.status(200);
+            res.json({message: `Group crée : ${groups.name}`});
+        }
+    });
         
-        let newGroups = new Groups(req.body);
-        newGroups.save((error, groups) => {
-            if(error){
-                res.status(401);
-                console.log(error);
-                res.json({message: "Rêquete invalide"});
-            }
-            else{
-                res.status(200);
-                res.json({message: `Group crée : ${groups.name}`});
-            }
-        });
-    }
+            
+}   
 
-}
 
 exports.getGroup = (req,res) =>{
     Groups.findById(req.params.groupId,(error,group) =>{
