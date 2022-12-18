ccccb31db1f7dea2187966eecd55f4300ec44f2f
Tania Olivia <tania.olivia69@yahoo.com>
2022-12-18 22:19:48 +0100

    add authentication access to addUsers and deleteUser in group

--- Back/src/api/routes/groupRoute.js
+++ Back/src/api/routes/groupRoute.js
@@ -1,5 +1,6 @@
 module.exports = (server) => {
     const groupController = require("../controllers/groupController");
+    const jwtMiddleware = require("../middlewares/jwtMiddleware");
     const cors = require('cors');
 
 server.post("/group/:userId", cors(), groupController.createGroup);
@@ -9,8 +10,8 @@ server.get("/groups", cors(), groupController.getAllGroups);
 server.route("/groups/:groupId")
 .all(cors())
 .get(groupController.getGroupById)
-.patch(groupController.addUsers)
-.delete(groupController.deleteGroupById);
+.patch(jwtMiddleware.authenticate,groupController.addUsers)
+.delete(jwtMiddleware.authenticate, groupController.deleteGroupById);
 
-server.patch("/groups/:groupId/deleteUsers", cors(), groupController.deleteUsers)
+server.patch("/groups/:groupId/deleteUsers", jwtMiddleware.authenticate, cors(), groupController.deleteUsers)
 }
