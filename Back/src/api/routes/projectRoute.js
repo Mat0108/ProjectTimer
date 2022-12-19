384681b04cad4419454e0a4c90813f95ea3b77a5
Coumbadiankha <coumba.diankha@hotmail.fr>
2022-12-19 01:43:34 +0100

    Update projectRoute.js

--- Back/src/api/routes/projectRoute.js
+++ Back/src/api/routes/projectRoute.js
@@ -1,8 +1,17 @@
 module.exports = (server) => {
     const projectController = require("../controllers/projectController");
+    const jwtMiddleware = require("../middlewares/jwtMiddleware");
     const cors = require('cors');
 
-server.post("/project/create", cors(), projectController.createProject);
+    server.post("/project/:groupId", cors(), projectController.createProject);
 
-
-}
+    server.get("/projects", cors(), projectController.getAllProjects);
+    
+    server.route("/projects/:projectId")
+    .all(cors())
+    .get(projectController.getProjectById)
+    .patch(jwtMiddleware.authenticate,projectController.addGroups)
+    .delete(jwtMiddleware.authenticate, projectController.deleteProjectById);
+    
+    server.patch("/projects/:projectId/deleteGroups", jwtMiddleware.authenticate, cors(), projectController.deleteGroups)
+    }
