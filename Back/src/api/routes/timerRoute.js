e8e6fe20ccfc1d83fa9b2c4e4d8bd802a8274a29
Tania Olivia <tania.olivia69@yahoo.com>
2022-12-19 01:04:02 +0100

    add getAllTimers

--- Back/src/api/routes/timerRoute.js
+++ Back/src/api/routes/timerRoute.js
@@ -8,4 +8,6 @@ server.patch("/timers/:timerId/pause", cors(), timerController.pauseTimer);
 server.patch("/timers/:timerId/resume", cors(), timerController.resumeTimer);
 server.patch("/timers/:timerId/stop", cors(), timerController.stopTimer);
 
+server.get("/timers", cors(), timerController.getAllTimers);
+
 }
