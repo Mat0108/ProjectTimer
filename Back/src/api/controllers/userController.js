44e021202159a7a6b099c0ce60eeb1f7ad42d6a9
Tania Olivia <tania.olivia69@yahoo.com>
2022-12-21 09:25:48 +0100

    add another populates and modify user schema

--- Back/src/api/controllers/userController.js
+++ Back/src/api/controllers/userController.js
@@ -1,5 +1,4 @@
 const User = require("../models/userModel");
-const Time = require("../models/timeModel");
 
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
@@ -153,7 +152,7 @@ exports.userLogout = (req, res, error) => {
 
 // Afficher tous les utilisateurs
 exports.getAllUsers = (req, res) => {
-    User.find({}).populate("loggedTimes").exec(function (error, users) {
+    User.find({}).populate("groups").populate("projects").populate("loggedTimes").exec(function (error, users) {
         if (error) {
             res.status(500);
             console.log(error);
@@ -169,7 +168,7 @@ exports.getAllUsers = (req, res) => {
 
 // Afficher un utilisateur par id
 exports.getUserById = (req, res) => {
-    User.findById(req.params.userId).populate("loggedTimes").exec(function (error, user) {
+    User.findById(req.params.userId).populate("groups").populate("projects").populate("loggedTimes").exec(function (error, user) {
         if (error) {
             res.status(401);
             res.json({ message: "Utilisateur connecté non trouvé" });
