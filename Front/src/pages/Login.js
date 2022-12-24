1674345f26482f4fdb54b044c64d6cb76877f741
Coumbadiankha <coumba.diankha@hotmail.fr>
2022-12-24 14:59:55 +0100

    Update Login.js
    Add const Login

--- Front/src/pages/Login.js
+++ Front/src/pages/Login.js
@@ -57,4 +57,22 @@ const Link = styled.a`
   text-decoration: underline;
   cursor: pointer;
 `;
+
+const Login = () => {
+    return (
+      <Container>
+        <Wrapper>
+          <Title>SIGN IN</Title>
+          <Form>
+            <Input placeholder="Username" />
+            <Input placeholder="Password" />
+            <Button>LOGIN</Button>
+            <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
+            <Link>CREATE A NEW ACCOUNT</Link>
+          </Form>
+        </Wrapper>
+      </Container>
+    );
+  };
+  
 export default Login;
