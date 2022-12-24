616cb87b25c965a9bd90eaaa7eda23ccdf32b194
Coumbadiankha <coumba.diankha@hotmail.fr>
2022-12-24 15:27:16 +0100

    Update Register.js
    Add const Container

--- Front/src/pages/Register.js
+++ Front/src/pages/Register.js
@@ -1,2 +1,16 @@
 import styled from "styled-components";
 import { mobile } from "../responsive";
+
+// CSS
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
