f29acf9c45a5fe6425048198391cd951f645bc8d
Coumbadiankha <coumba.diankha@hotmail.fr>
2022-12-24 20:13:39 +0100

    Update auth.js
    Add const newUser

--- Back/src/api/routes/auth.js
+++ Back/src/api/routes/auth.js
@@ -4,6 +4,14 @@ const CryptoJS = require("crypto-js");
 
 //REGISTER
 router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt(
+          req.body.password,
+          process.env.PASS_SEC
+        ).toString(),
+      });
 
-    
+
 });
