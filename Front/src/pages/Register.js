621cc6ca49ccc616f7952fb56b123395e87236ac
Coumbadiankha <coumba.diankha@hotmail.fr>
2022-12-24 20:11:01 +0100

    Update Register.js
    Add const Input Agreement, Button, Register (CSS + Function)

--- Front/src/pages/Register.js
+++ Front/src/pages/Register.js
@@ -33,3 +33,50 @@ const Form = styled.form`
   flex-wrap: wrap;
 `;
 
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
+
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+`;
+
+const Register = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>
+        <Form>
+          <Input placeholder="Firstname" />
+          <Input placeholder="Lastname" />
+          <Input placeholder="Username" />
+          <Input placeholder="Email" />
+          <Input placeholder="Confirm Email" />
+          <Input placeholder="Password" />
+          <Input placeholder="Confirm Password" />
+          <Agreement>
+            By creating an account, I consent to the processing of my personal
+            data in accordance with the <b>PRIVACY POLICY</b>
+          </Agreement>
+          <Button>CREATE</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Register;
+
