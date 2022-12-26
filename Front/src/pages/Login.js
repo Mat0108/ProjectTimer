78d55f32265f226be39f47979815c3283cb7e6f0
Coumbadiankha <coumba.diankha@hotmail.fr>
2022-12-27 00:32:40 +0100

    Update Login.js

--- Front/src/pages/Login.js
+++ Front/src/pages/Login.js
@@ -39,31 +39,42 @@ const Login = () => {
     }
 
     return (
-      <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
-          <div className='hidden sm:block'>
-              <img className='w-full h-full object-cover'  alt="" />
-          </div>
-  
-          <div className='bg-gray-800 flex flex-col justify-center'>
-              <form className='max-w-[400px] w-full mx-auto rounded-lg bg-black-900 p-8 px-8'>
-                  <h2 className='text-4xl dark:text-white font-bold text-center'>LOGIN</h2>
-                  <div className='flex flex-col text-gray-400 py-2'>
-                      <label>Email</label>
-                      <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-black-800 focus:outline-none' type="text" />
-                  </div>
-                  <div className='flex flex-col text-gray-400 py-2'>
-                      <label>Password</label>
-                      <input className='p-2 rounded-lg bg-gray-700 mt-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="password" />
-                  </div>
-                  <div className='flex justify-between text-gray-400 py-2'>
-                      <p className='flex items-center'><input className='mr-2' type="checkbox" /> Remember Me</p>
-                      <p>Forgot Password</p>
-                  </div>
-                  <button className='w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg'>Connexion</button>
-                  
-              </form>
-          </div>
-      </div>
+        <div>
+            { messages.map(message => <div class={message.type}>
+                {message.msg}
+            </div> )}
+            <form onSubmit={onClick}>
+                <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
+                    <div className='hidden sm:block'>
+                        <img className='w-full h-full object-cover' src="/images/rabbit.png" alt="image" />
+                    </div>
+            
+                    <div className='bg-black flex flex-col justify-center'>
+                        <form className='max-w-[400px] w-full mx-auto rounded-lg bg-black-900 p-8 px-8'>
+                            <h2 className='text-4xl dark:text-white font-bold text-center'>LOGIN</h2>
+
+                            <div className='flex flex-col text-gray-400 py-2'>
+                                <label>Email</label>
+                                <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-black-800 focus:outline-none' type="text" onChange={onChangeHandler} value={user.email} class="form-control Cinput" placeholder="Entrez votre email "id="email"/>
+                            </div>
+
+                            <div className='flex flex-col text-gray-400 py-2'>
+                                <label>Password</label>
+                                <input className='p-2 rounded-lg bg-gray-700 mt-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="password" onChange={onChangeHandler} value = {user.password} class="form-control Cinput" placeholder="Entrez votre password" id="password" />
+                            </div>
+
+                            <div className='flex justify-between text-gray-400 py-2'>
+                                <p className='flex items-center'><input className='mr-2' type="checkbox" /> Remember Me</p>
+                                <p>Forgot Password</p>
+                            </div>
+
+                            <button className='w-full my-5 py-2 bg-blue shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg'>Connexion</button>
+                            <Link to='/Register' class="Clink">Je n'ai pas de compte malheureusement...</Link>
+                        </form>
+                    </div>
+                </div>
+            </form>
+        </div>
     )
   }
 
