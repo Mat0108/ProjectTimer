607bcccb47095a932923898e54a14c22fb09e85c
Coumbadiankha <coumba.diankha@hotmail.fr>
2022-12-26 23:01:32 +0100

    update Login.js

--- Front/src/pages/Login.js
+++ Front/src/pages/Login.js
@@ -1,6 +1,43 @@
-import React,{useState}  from 'react';
+import React,{useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {login} from "../services/auth";
 
-export default function Login() {
+const Login = () => {
+    let navigate = useNavigate();
+    const [messages,setMessages] = useState([]);
+    const [user,setUser] = useState({
+        'email':'',
+        'password':''
+    });
+
+    const onClick = async (event) =>{
+        event.preventDefault();
+        console.log(user);
+        setMessages([])
+        if(user.password !== "" && user.email !== ""){
+            try{
+                await login(user);
+                setMessages([...messages,{type:"alert alert-success",msg:"vous êtes connecté !"}]);
+            
+                
+                //navigate("/category/2"); 
+            }catch (error){
+                if (error.response){
+                    setMessages([...messages,{type:"alert alert-danger",msg:error.response.data}]);
+                }else{
+                    setMessages([...messages,{type:"alert alert-danger",msg:"erreur : "+error.message}]);
+                }
+            }
+        }else{
+            setMessages([...messages,{type:"alert alert-info",msg:"merci de remplir les deux champs "}]);
+        }
+    }
+
+    const onChangeHandler = (event) =>{
+        const {id, value}= event.target
+        setUser({...user, [id]:value})
+    }
+
     return (
       <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
           <div className='hidden sm:block'>
@@ -8,11 +45,11 @@ export default function Login() {
           </div>
   
           <div className='bg-gray-800 flex flex-col justify-center'>
-              <form className='max-w-[400px] w-full mx-auto rounded-lg bg-gray-900 p-8 px-8'>
+              <form className='max-w-[400px] w-full mx-auto rounded-lg bg-black-900 p-8 px-8'>
                   <h2 className='text-4xl dark:text-white font-bold text-center'>LOGIN</h2>
                   <div className='flex flex-col text-gray-400 py-2'>
                       <label>Email</label>
-                      <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="text" />
+                      <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-black-800 focus:outline-none' type="text" />
                   </div>
                   <div className='flex flex-col text-gray-400 py-2'>
                       <label>Password</label>
@@ -22,10 +59,12 @@ export default function Login() {
                       <p className='flex items-center'><input className='mr-2' type="checkbox" /> Remember Me</p>
                       <p>Forgot Password</p>
                   </div>
-                  <button className='w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg'>SIGNIN</button>
+                  <button className='w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg'>Connexion</button>
                   
               </form>
           </div>
       </div>
     )
   }
+
+  export default Login;
