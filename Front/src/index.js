fb4eb4185761667b159f197e35d5efa08922f32a
Matthieu B <mesjeux0108@gmail.com>
2022-12-27 00:48:12 +0100

    page group et menu

--- Front/src/index.js
+++ Front/src/index.js
@@ -12,17 +12,26 @@ import './index.css';
 
 const App =() => {
 
-  return <div className='bg-lightgrey w-screen h-screen text-white'>
-          
+  return <div className='bg-lightgrey w-screen h-screen text-white flex flex-col'>
+         
+          {/* <div className='w-screen'><Nav /></div>  */}
           <Router>
-            <div><Nav /></div> 
-              <Routes>
+            <div className='w-full'><Nav /></div> 
+            <div className='w-full h-full flex flex-row'> 
+               
+              <div className='w-[250px] h-full bg-lightgrey'></div>
+              <div className='w-full h-full '>
+                <Routes>
                   {/* <Route path="/" element={<Home/>}></Route> */}
                   {/* <Route path="/Login" element ={<Login />}></Route>
                   <Route path="/Register" element={<Register />}></Route> */}
                   <Route path="/Groups" element={<Groups/>}></Route>
                   <Route path="/Groups/:groupId" element={<Group/>}></Route>
-                  </Routes>
+                </Routes>
+              </div>
+            </div>
+           
+              
           </Router>
       </div>
       
