f33fa3b710cf106138b27a8ff93e029022db0f14
Tania Olivia <tania.olivia69@yahoo.com>
2022-12-27 04:53:07 +0100

    modify group controller, model and route

--- Back/src/api/controllers/groupController.js
+++ Back/src/api/controllers/groupController.js
@@ -1,9 +1,6 @@
 const Group = require("../models/groupModel");
 const User = require("../models/userModel");
-
-const bcrypt = require("bcrypt");
-const axios = require("axios");
-const jwt = require("jsonwebtoken");
+const Project = require("../models/projectModel");
 
 // Créer un nouveau groupe
 exports.createGroup = async (req, res) => {  
@@ -22,12 +19,12 @@ exports.createGroup = async (req, res) => {
                         res.json({ message: "Utilisateur non trouvé" });
                     }
                     else {
-                       
                         let newGroup = new Group({
                             name: req.body.name, 
                             users: users.map(e=>{return e._id}), 
                             admin: admin._id
                         }); 
+
                         newGroup.save((error, group) => {
                             if(error){
                                 res.status(401);
@@ -37,14 +34,18 @@ exports.createGroup = async (req, res) => {
                             else{
                                 users.map(e=>{
                                     let groupUsers = e.groups;
-                                    groupUsers.push(group._id);    
+                                    groupUsers.push(group._id);  
+
                                     User.findByIdAndUpdate({ _id: e._id },{groups: groupUsers}, { new: true })
                                     .then(result => console.log('result : ', result))
                                     .catch((error) => console.log('error : ', error))
-
-                                    
-                                    
                                 })
+
+                                admin.groups.push(group._id)
+                                User.findByIdAndUpdate({ _id: admin._id },{groups: admin.groups}, { new: true })
+                                .then(result => console.log('result : ', result))
+                                .catch((error) => console.log('error : ', error))
+
                                 res.status(200);
                                 res.json({message: `Groupe crée : ${group.name}`, groupData: newGroup});
                             }
@@ -52,19 +53,31 @@ exports.createGroup = async (req, res) => {
                     }
                 })
             }
-        }
-
-   
-
-
-
-    })
+            else {
+                let newGroup = new Group({
+                    name: req.body.name, 
+                    admin: admin._id
+                }); 
     
+                newGroup.save((error, group) => {
+                    if(error){
+                        res.status(401);
+                        console.log(error);
+                        res.json({message: newGroup});
+                    }
+                    else{
+                        res.status(200);
+                        res.json({message: `Groupe crée : ${group.name}`, groupData: newGroup});
+                    }
+                })
+            }
+        }
+    })
 }   
 
 // Afficher tous les groupes
 exports.getAllGroups = (req, res) => {
-    Group.find({}).populate("users").populate("admin").exec(function (error, groups){
+    Group.find({}).populate("users").populate("admin").populate("projects").exec(function (error, groups){
         if(error){
             res.status(500);
             console.log(error);
@@ -79,7 +92,7 @@ exports.getAllGroups = (req, res) => {
 
 // Afficher un groupe par id
 exports.getGroupById = (req, res) => {
-    Group.findById(req.params.groupId).populate("users").populate("admin").exec(function (error, group){
+    Group.findById(req.params.groupId).populate("users").populate("admin").populate("projects").exec(function (error, group){
         if(error){
             res.status(500);
             console.log(error);
@@ -94,13 +107,67 @@ exports.getGroupById = (req, res) => {
 
 // Supprimer un groupe par id
 exports.deleteGroupById = (req, res) => {
-    Group.findByIdAndDelete(req.params.groupId, (error, group) =>{
+    Group.findByIdAndDelete(req.params.groupId).populate("users").populate("admin").populate("projects").exec((error, group) =>{
         if(error){
             res.status(500);
             console.log(error);
             res.json({message: "Groupe non trouvé"});
         }
         else{
+            if(group.users != null){
+                group.users.map(user => {
+                    let userGroups = user.groups.filter(group => group.valueOf() !== req.params.groupId);
+                    User.findByIdAndUpdate({_id: user._id}, {groups: userGroups}, {new: true}, (error, userUpdate) =>{})
+
+                    group.projects.map(project => {
+                        if(user.projects.includes(project._id)){
+                            console.log(user.projects)
+                            console.log("user.projects")
+                            let newProjects = user.projects.filter(userProject => userProject.valueOf() !== project._id.valueOf());
+
+                            console.log(newProjects)
+                            User.findByIdAndUpdate({_id: user._id}, {projects: newProjects}, {new: true}, (error, adminUpdate) =>{})
+                        }
+                        
+                    })
+                })
+            }
+
+            if(group.projects != null){
+                group.projects.map(project => {  
+                    let projectGroups = project.groups.filter(group => group.valueOf() !== req.params.groupId);
+                    Project.findByIdAndUpdate({_id: project._id}, {groups: projectGroups}, {new: true}, (error, userUpdate) =>{})                   
+                })
+            }
+
+            User.findById({_id: group.admin._id}, (error, admin) =>{
+                if(admin.groups != null) {
+                    let adminGroups = admin.groups.filter(group => group.valueOf() !== req.params.groupId);
+                    User.findByIdAndUpdate({_id: group.admin}, {groups: adminGroups }, {new: true}, (error, adminUpdate) =>{})
+                }
+
+                if(admin.projects != null){
+                    admin.projects.map(projectId => {
+                        Project.findById({_id: projectId}, (error, project) => {
+                            project.groups.map(group => {
+                                if(project.groups.includes(req.params.groupId)){
+                                    let newGroups = group.filter(groupId => groupId.valueOf() !== req.params.groupId);
+                                
+                                    let updateProject = {
+                                        _id: adminProject._id,
+                                        name: adminProject.name,
+                                        groups: newGroups,
+                                        __v: adminProject.__v
+                                    }
+    
+                                    User.findByIdAndUpdate({_id: group.admin}, {projects: updateProject}, {new: true}, (error, adminUpdate) =>{})
+                                }
+                            })
+                        })
+                    })
+                }
+            })
+            
             res.status(200);
             res.json({message: `Groupe supprimé: ${group.name}`});
         }
@@ -124,15 +191,20 @@ exports.addUsers = (req, res) => {
                 }
                 else {
                     let groupUsers = group.users;
-                    users.map(e=>{if(!groupUsers.includes(e._id)){
-                        groupUsers.push(e._id);
-                        User.findByIdAndUpdate({ _id: e._id },{groups: group._id}, { new: true })
-                        .then(result => console.log('result : ', result))
-                        .catch((error) => console.log('error : ', error))
-                    }})
-                    Group.findByIdAndUpdate({_id: req.params.groupId}, {users: groupUsers}, {new: true}, (error, groupUpdate) => {});
+                    users.map(e => {
+                        if(!groupUsers.includes(e._id)){
+                            groupUsers.push(e._id);
 
-                }})
+                            User.findByIdAndUpdate({ _id: e._id },{groups: group._id}, { new: true })
+                            .then(result => console.log('result : ', result))
+                            .catch((error) => console.log('error : ', error))
+                        }
+                    })
+
+                    Group.findByIdAndUpdate({_id: req.params.groupId}, {users: groupUsers}, {new: true}, (error, groupUpdate) => {});
+                }
+            })
+
             res.status(200);
             res.json({message: `${group.name} est bien modifié`})
         }
@@ -156,19 +228,23 @@ exports.deleteUsers = (req, res) => {
                 }
                 else {
                     let groupUsers = group.users;
-                    users.map(e=>{
-                        if(groupUsers.includes(e._id)){
-                        
+                    users.map(e => {
+                        if(groupUsers.includes(e._id)) {
                             let groupUser = e.groups;
-                            groupUser = groupUser.filter(group1=> group1 != group._id)
+                            groupUser = groupUser.filter(group1 => group1.valueOf() !== group._id.valueOf())
+
                             User.findByIdAndUpdate({ _id: e._id },{groups: groupUser}, { new: true })
                             .then(result => console.log('result : ', result))
                             .catch((error) => console.log('error : ', error))
-                     }})
-                    groupUsers = groupUsers.filter(group1 => !users.includes(group1))
-                    Group.findByIdAndUpdate({_id: req.params.groupId}, {users: groupUsers}, {new: true}, (error, groupUpdate) => {});
+                        }
 
-                }})
+                        groupUsers = groupUsers.filter(user => user._id.valueOf() !== e._id.valueOf())
+                    
+                        Group.findByIdAndUpdate({_id: req.params.groupId}, {users: groupUsers}, {new: true}, (error, groupUpdate) => {});
+                    })
+                }
+            })
+
             res.status(200);
             res.json({message: `${group.name} est bien modifié`})
         }
