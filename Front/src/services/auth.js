02543aef9478ca9b2964310a18012e9e96b66496
Coumbadiankha <coumba.diankha@hotmail.fr>
2022-12-28 00:08:15 +0100

    Update auth.js
    Modification des axios post

--- Front/src/services/auth.js
+++ Front/src/services/auth.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 export const register = async (user) => {
-    const res = await axios.post(`http://localhost:3000/register`,user);
+    const res = await axios.post(`http://localhost:3000/user/register`,user);
     return res;
     
 }
 export const login = async (user) => {
-    const res = await axios.post(`http://localhost:3000/login`,user);
+    const res = await axios.post(`http://localhost:3000/user/login`,user);
     return res;
     
 }
