b87e7698a9544da9af1f2fb9e053261853f06be3
Coumbadiankha <coumba.diankha@hotmail.fr>
2022-12-28 00:11:33 +0100

    Update Register.js
    Add const Register

--- Front/src/pages/Register.js
+++ Front/src/pages/Register.js
@@ -1,3 +1,23 @@
 import React,{useState} from "react";
 import { Link, useNavigate } from "react-router-dom";
 import {register} from "../services/auth";
+
+const Register = () => {
+    let navigate = useNavigate();
+    const [user,setUser] = useState({
+        
+        'firstname':'',
+        'lastname':'',
+
+        'email':'',
+        'cf_email':'',
+
+        'password':'',
+        'cf_password':''
+        
+    })
+}
+   
+
+
+export default Register;
