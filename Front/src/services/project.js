47b4d6b50423a899cac6a07eadbc33428beb536b
nihelouanassi <nihelouanasi@gmail.com>
2022-12-28 16:01:46 +0100

    getProjectById-service

--- Front/src/services/project.js
+++ Front/src/services/project.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
-export const getProjects=async ()=>{
+export const getAllProjects=async ()=>{
     const res =await axios.get(`http://localhost:3000/projects/`); 
+
+    return res.data;
+}
+
+export const getProjectById= async (projectId)=>{
+    const res = await axios.get(`http://localhost:3000/projects/${projectId}`);
+    
+    return res.data;
 }
