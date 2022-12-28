48f18bb5078d6e4b55dcc146a981f50e17129455
Coumbadiankha <coumba.diankha@hotmail.fr>
2022-12-28 22:19:31 +0100

    Update Login.js

--- Front/src/pages/Login.js
+++ Front/src/pages/Login.js
@@ -54,12 +54,12 @@ const Login = () => {
                         <form className='max-w-[400px] w-full mx-auto rounded-lg bg-black-900 p-8 px-8'>
                             <h2 className='text-4xl dark:text-white font-bold text-center'>LOGIN</h2>
 
-                            <div className='flex flex-col text-gray-400 py-2'>
+                            <div className='flex flex-col text-gray-400 py-2 mb-2'>
                                 <label>Email</label>
                                 <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-black-800 focus:outline-none' type="text" onChange={onChangeHandler} value={user.email} class="form-control Cinput" placeholder="Enter your email "id="email"/>
                             </div>
 
-                            <div className='flex flex-col text-gray-400 py-2'>
+                            <div className='flex flex-col text-gray-400 py-2 mb-2'>
                                 <label>Password</label>
                                 <input className='p-2 rounded-lg bg-gray-700 mt-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type="password" onChange={onChangeHandler} value = {user.password} class="form-control Cinput" placeholder="Enter your password" id="password" />
                             </div>
