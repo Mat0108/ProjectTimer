3e5a0d485b3becb488411bd6b34a062ee27ad33c
Matthieu B <mesjeux0108@gmail.com>
2022-12-28 22:57:18 +0100

    update index.js

--- Front/src/index.js
+++ Front/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router,useLocation  } from 'react-router-dom';
 import {Route,Routes} from 'react-router';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -10,25 +10,31 @@ import Nav from './pages/Nav';
 import './index.css';
 import { ModalProvider } from "./containers/Modal";
 const App =() => {
-
+  function getPage(elem){
+    return <div className='relative w-full h-[calc(100%-50px)] flex flex-row'> 
+      <div className='w-[250px] h-full  '></div>
+      <div className='w-[calc(100%-250px)] h-full bg-gray-gainsboro rounded-tl-[30px]'>
+      {elem}
+      </div>
+      </div>
+  }
+  function getLR(elem){
+    return <div className='bg-gray-gainsboro'>{elem}</div>
+  }
   return <div className='app bg-gray-silver w-screen h-screen text-white flex flex-col'>
          <ModalProvider>
           {/* <div className='w-screen'><Nav /></div>  */}
           <Router>
             <div className='w-full'><Nav /></div> 
-            <div className='relative w-full h-[calc(100%-50px)] flex flex-row'> 
-               
-              <div className='w-[250px] h-full  '></div>
-              <div className='w-[calc(100%-250px)] h-full bg-grey-circle rounded-tl-[30px]'>
-                <Routes>
-                  {/* <Route path="/" element={<Home/>}></Route> */}
-                  <Route path="/Login" element ={<Login />}></Route>
-                  <Route path="/Register" element={<Register />}></Route>
-                  <Route path="/Groups" element={<Groups/>}></Route>
-                  <Route path="/Groups/:groupId" element={<Group/>}></Route>
-                </Routes>
-              </div>
-            </div>
+           
+            <Routes>
+              {/* <Route path="/" element={<Home/>}></Route> */}
+              <Route path="/Login" element ={<Login />}></Route>
+              <Route path="/Register" element={<Register />}></Route>
+              <Route path="/Groups" element={getPage(<Groups/>)}></Route>
+              <Route path="/Groups/:groupId" element={getPage(<Group/>)}></Route>
+            </Routes>
+
            
               
           </Router>
