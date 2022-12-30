95c486ee740553326372eba0805fbb03ac8fd409
Matthieu B <mesjeux0108@gmail.com>
2022-12-31 00:21:04 +0100

    udpage group view, api action

--- Front/src/pages/menu.js
+++ Front/src/pages/menu.js
@@ -5,7 +5,7 @@ const Menu = ()=>{
     const timer = <img className="mr-3"src="/images/timer.png" alt="image" width={40} height={20} ></img>
     return <div className="flex flex-col text-white mt-[90%]">
         <div><Link className="text-2xl flex hover:bg-white hover:bg-opacity-20 py-4 px-8" to="/Groups">{group} Groups </Link></div>
-        <div><Link className="text-2xl flex hover:bg-white hover:bg-opacity-20 py-4 px-8" to="/Projects=">{project} Projects </Link></div>
+        <div><Link className="text-2xl flex hover:bg-white hover:bg-opacity-20 py-4 px-8" to="/Projects">{project} Projects </Link></div>
         <div><Link className="text-2xl flex hover:bg-white hover:bg-opacity-20 py-4 px-8" to="/TimeTracker">{timer} Time Tracker </Link></div>
     </div>
 }
