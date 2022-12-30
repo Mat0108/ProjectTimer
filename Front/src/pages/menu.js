a787a26f2f196e24895476e51dc9bab9837b1152
Tania Olivia <tania.olivia69@yahoo.com>
2022-12-30 03:18:59 +0100

    update menu

--- Front/src/pages/menu.js
+++ Front/src/pages/menu.js
@@ -4,9 +4,9 @@ const Menu = ()=>{
     const project = <img className="mr-3"src="/images/project.png" alt="image" width={40} height={20} ></img>
     const timer = <img className="mr-3"src="/images/timer.png" alt="image" width={40} height={20} ></img>
     return <div className="flex flex-col  text-white gap-8 ml-3 mt-[90%] ">
-        <div ><Link className="text-3xl flex" to="/Groups">{group} Group </Link></div>
-        <div ><Link className="text-3xl flex" to="/=">{project} Project </Link></div>
-        <div ><Link className="text-3xl flex" to="/=">{timer} Timer </Link></div>
+        <div ><Link className="text-3xl flex" to="/Groups">{group} Groups </Link></div>
+        <div ><Link className="text-3xl flex" to="/=">{project} Projects </Link></div>
+        <div ><Link className="text-3xl flex" to="/TimeTracker">{timer} Time Tracker </Link></div>
     </div>
 }
 export default Menu
