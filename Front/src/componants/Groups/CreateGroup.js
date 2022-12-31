22a1b6154e35000deecd3451a92d38b9a3db94b8
Tania Olivia <tania.olivia69@yahoo.com>
2022-12-31 01:40:47 +0100

    edit design

--- Front/src/componants/Groups/CreateGroup.js
+++ Front/src/componants/Groups/CreateGroup.js
@@ -32,44 +32,44 @@ const CreateGroup =()=>{
         }
     }
     return (
-        <div className='relative w-[500px] h-[500px] bg-gray-normal rounded-xl grid grid-rows-creategroup'>
+        <div className='relative w-[500px] h-[500px] bg-gray-normal rounded-xl grid grid-rows-creategroup text-white'>
             <div className='row-start-1'>
-                <h1 className='text-3xl text-center mt-2'>Créer un groupe</h1>
+                <h1 className='text-3xl text-center mt-7 font-bold'>CREATE A GROUP</h1>
             </div>
-            <div className="row-start-2">
-                <label className='ml-[38px]'>Nom du groupe</label>
+            <div className="row-start-2 mt-8">
+                <label className='ml-[38px]'>Group name :</label>
                 <input className='ml-[38px] w-[420px] rounded-md text-black bg-white2 mt-2 p-2 ' type="text" value={name} onChange={e=>setName(e.target.value)} />
                             
             </div>
-            {users && <><div className="row-start-3">
-            <label className='ml-[40px]'>Admin</label>
+            {users && <><div className="row-start-3 mt-8">
+            <label className='ml-[40px]'>Admin :</label>
             <Select
                     defaultValue={""}
                     name="colors"
                     options={users}
-                    className="basic-multi-select bg-gray-normal ml-[22px] w-[450px] text-black px-4 border-solid "
+                    className="basic-multi-select bg-gray-normal ml-[22px] w-[450px] text-black px-4 border-solid mt-2"
                     classNamePrefix="select text-white"
                     onChange={e=>setAdmin(e)}
                 /> 
             </div>
-            <div className="row-start-4">
+            <div className="row-start-4 mt-10">
             <label className='ml-[40px]'>Users</label>
                  <Select
                     defaultValue={""}
                     isMulti
                     name="colors"
                     options={users}
-                    className="basic-multi-select bg-gray-normal ml-[22px] w-[450px] text-black px-4 border-solid "
+                    className="basic-multi-select bg-gray-normal ml-[22px] w-[450px] text-black px-4 border-solid mt-2"
                     classNamePrefix="select text-white"
                     onChange={e=>setListusers(e)}
                 />
             </div></>}
-            <div className='row-start-5'><div className="space-x-5 ml-[22%]">
-          <button onClick={()=>{modalChange(<div></div>);displayModalChange(false)}} className="min-w-[30%] text-red bg-white2 border-2 border-red px-6 py-2 rounded-3xl" type="button">
-          <span className="font-[AvenirNextCyrDemi]">Annuler</span>
+            <div className='row-start-5 mt-12'><div className="space-x-5 ml-[22%]">
+          <button onClick={()=>{modalChange(<div></div>);displayModalChange(false)}} className="min-w-[30%] text-white font-bold bg-red hover:text-red hover:bg-white border-2 border-red px-6 py-2 rounded-3xl" type="button">
+          <span>Annuler</span>
           </button>
-          <button onClick={()=>CreateGroup()} className="min-w-[30%] text-green bg-white2 border-2 border-green px-6 py-2 rounded-3xl " type="button">
-            <span className="font-[AvenirNextCyrDemi] ">Confirmer</span>
+          <button onClick={()=>CreateGroup()} className="min-w-[30%] text-white font-bold bg-green hover:text-green hover:bg-white border-2 border-green px-6 py-2 rounded-3xl " type="button">
+            <span>Confirmer</span>
           </button>
         </div></div>
         </div>
