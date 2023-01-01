46c670f4892474bb91ec6538b7bb71c664ec7a42
nihelouanassi <nihelouanasi@gmail.com>
2023-01-01 17:07:44 +0100

    test-createProject

--- Front/src/componants/Projects/CreateProject.js
+++ Front/src/componants/Projects/CreateProject.js
@@ -94,7 +94,7 @@ const CreateProject = () => {
     const CreateProject = async() =>{
         setProject({
             
-            admin: admin.value,
+            admin: localStorage.getItem("userId"),
             name:name
 
         });
@@ -124,24 +124,8 @@ const CreateProject = () => {
                     <input className='ml-[38px] w-[420px] rounded-md text-black bg-white2 mt-2 p-2 ' type="text" name="name" value={name} onChange={e=>setName(e.target.value)} />
 
                 </div>
-                {users && <><div className="row-start-3">
-                    <label className='ml-[40px]'>Admin</label>
-                    {<Select
-
+                
                     
-
-                        options={users}
-                        className="basic-multi-select bg-gray-normal ml-[22px] w-[450px] text-black px-4 border-solid "
-                        classNamePrefix="select text-white"
-                        name="admin"
-                       
-                        onChange={e=>setAdmin(e)}
-                    />}
-                </div>
-                    <div className="row-start-4">
-                        {/* <label className='ml-[40px]'>Timer</label> */}
-
-                    </div></>}
                 <div className='row-start-5'><div className="space-x-5 ml-[22%]">
                     <button onClick={() => { modalChange(<div></div>); displayModalChange(false); }} className="min-w-[30%] text-red bg-white2 border-2 border-red px-6 py-2 rounded-3xl" type="button">
                         <span className="font-[AvenirNextCyrDemi]">Annuler</span>
