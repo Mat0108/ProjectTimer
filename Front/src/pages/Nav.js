958fdd117bd06ab3cd5bc778e9472a357575a859
Coumbadiankha <coumba.diankha@hotmail.fr>
2023-01-01 21:16:38 +0100

    Update Nav.js

--- Front/src/pages/Nav.js
+++ Front/src/pages/Nav.js
@@ -22,7 +22,7 @@ const Nav = () =>{
                             <button className="font-bold" onClick={()=>setDropdown(!dropdown)}>{localStorage.getItem("userFirstname")} &nbsp; ▼ </button>
                             {dropdown && 
                                 <div className='absolute top-8 rigth-0 flex flex-col bg-white z-[999] rounded-b-lg text-black'>
-                                    <div className='hover:bg-black hover:bg-opacity-25 py-2 px-8 hover:text-white'>Logout</div>
+                                    <div onClick className='hover:bg-black hover:bg-opacity-25 py-2 px-8 hover:text-white'>Logout</div>
                                 </div>}
                         </div>
                         :
