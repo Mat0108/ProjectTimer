118405e18e388bbaf8ccd2dbe052f02ecaa3631e
Coumbadiankha <coumba.diankha@hotmail.fr>
2023-01-01 21:17:04 +0100

    Update Register.js

--- Front/src/pages/Register.js
+++ Front/src/pages/Register.js
@@ -3,17 +3,14 @@ import { Link, useNavigate } from "react-router-dom";
 import {register} from "../services/user";
 
 const Register = () => {
-    let navigate = useNavigate();
     const [user,setUser] = useState({
         
         'firstname':'',
         'lastname':'',
 
         'email':'',
-        'cf_email':'',
 
-        'password':'',
-        'cf_password':''
+        'password':''
         
     })
 
@@ -25,8 +22,7 @@ const Register = () => {
             user.password.length >= 10 && user.email !== ""){
             const res = await register(user);
             if (res.status === 200)
-            {
-                //navigate("/category/2"); 
+            { 
             }
             console.log(res);
         }else{
@@ -72,18 +68,8 @@ const Register = () => {
                             </div>
 
                             <div className='flex flex-col text-black py-1 mb-2'>
-                                <label className="py-1">Confirm Email :</label>
-                                <input className='rounded-lg bg-gray-700 mt-2 py-2 px-4 focus:border-blue-500 focus:bg-black-800 focus:outline-none mb-2 form-control Cinput' type="text" onChange={onChangeHandler} value={user.cf_email} placeholder="Confirm email"id="cf_email"/>
-                            </div>
-
-                            <div className='flex flex-col text-black py-1 mb-2'>
                                 <label className="py-1">Password :</label>
                                 <input className='p-2 rounded-lg bg-gray-700 mt-2 py-2 px-4 focus:border-blue-500 focus:bg-gray-800 focus:outline-none form-control Cinput' type="password" onChange={onChangeHandler} value = {user.password} placeholder="Enter your password" id="password" />
-                            </div>
-
-                            <div className='flex flex-col text-black py-1 mb-2'>
-                                <label className="py-1">Confirm Password :</label>
-                                <input className='p-2 rounded-lg bg-gray-700 mt-2 py-2 px-4 focus:border-blue-500 focus:bg-gray-800 focus:outline-none form-control Cinput' type="password" onChange={onChangeHandler} value = {user.cf_password} placeholder="Confirm password" id="cf_password" />
                             </div>
 
                             <button onSubmit={onClick}
