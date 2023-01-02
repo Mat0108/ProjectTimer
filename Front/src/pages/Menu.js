2e990bac7b4b1f2670afd0886f1b45e87f21dc46
Tania Olivia <tania.olivia69@yahoo.com>
2023-01-02 21:50:41 +0100

    add spacing to menu

--- Front/src/pages/Menu.js
+++ Front/src/pages/Menu.js
@@ -4,9 +4,9 @@ import { Group, Project, Timer } from '../componants/Image/Image';
 const Menu = ()=>{    
     return (
         <div className="flex flex-col text-white mt-[90%]">
-            <div><Link className="text-2xl flex hover:bg-white hover:bg-opacity-20 py-4 px-8" to="/Groups"><Group size={[40,40]}/> Groups </Link></div>
-            <div><Link className="text-2xl flex hover:bg-white hover:bg-opacity-20 py-4 px-8" to="/Projects"><Project size={[40,40]}/> Projects </Link></div>
-            <div><Link className="text-2xl flex hover:bg-white hover:bg-opacity-20 py-4 px-8" to="/TimeTracker"><Timer size={[40,40]}/> Time Tracker </Link></div>
+            <div><Link className="text-2xl flex hover:bg-white hover:bg-opacity-20 py-4 px-8" to="/Groups"><Group size={[40,40]}/> &nbsp; Groups </Link></div>
+            <div><Link className="text-2xl flex hover:bg-white hover:bg-opacity-20 py-4 px-8" to="/Projects"><Project size={[40,40]}/> &nbsp; Projects </Link></div>
+            <div><Link className="text-2xl flex hover:bg-white hover:bg-opacity-20 py-4 px-8" to="/TimeTracker"><Timer size={[40,40]}/> &nbsp; Time Tracker </Link></div>
         </div>
     )
 }
