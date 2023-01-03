6b038b7604c031b9239de7c1a52f6bdb72785640
Coumbadiankha <coumba.diankha@hotmail.fr>
2023-01-03 11:33:52 +0100

    Update Swagger

--- Back/src/app.js
+++ Back/src/app.js
@@ -2,15 +2,34 @@ const express = require('express');
 const cors = require('cors');
 
 //SWAGGER (Documentation)
-//const swaggerJSDoc = require('swagger-jsdoc');
-//const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const hostname = "0.0.0.0";
 const port = 3000;
-
 const server = express();
-
 const mongoose = require("mongoose");
+
+const swaggerOptions={
+    definition:{
+        openapi:'3.0.0',
+        info:{
+            title:'Employee Management API',
+            version:'1.0.0',
+            description:'Employe Api for employee management',
+            contact:{
+                name:'Jayaramachandran Augustin',
+                url:'https://whizpath.com',
+                email:'jayaramachandran@whizpath.com'
+            },
+            servers:["http://localhost:3000"]
+        }
+    },
+    apis:["app.js"]
+}
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions)
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 process.env.TZ = 'Europe/Paris'
  
@@ -35,6 +54,7 @@ const userRoute = require("./api/routes/userRoute");
 const groupRoute = require("./api/routes/groupRoute");
 const projectRoute = require("./api/routes/projectRoute");
 const timeRoute = require("./api/routes/timeRoute");
+const { application } = require('express');
 
 userRoute(server);
 groupRoute(server);
