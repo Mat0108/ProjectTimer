9fe1647e839c8087386a20f9b3e9031390c81e18
nihelouanassi <nihelouanasi@gmail.com>
2023-01-03 10:17:22 +0100

    fix-css-project

--- Front/src/pages/Projects.js
+++ Front/src/pages/Projects.js
@@ -88,7 +88,9 @@ const Projects = () => {
 
     const edit = <img src="/images/editer.png" alt="image" width={20} height={20} ></img>
     const view = <img src="/images/view.png" alt="image" width={20} height={20} ></img>
-    const bin = <img src="/images/bin.png" alt="image" width={20} height={20} ></img>
+    const bin = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 25 25" strokeWidth="1.5" stroke="currentColor" className="w-7 h-7">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+    </svg>
     function getButton(color, text, onclickvar) {
         return <div><button className={`p-2 ${color} rounded-xl`} onClick={onclickvar}>{text}</button></div>
     }
@@ -174,9 +176,9 @@ const Projects = () => {
 
 
                             <td className='px-4 py-2'><div className='grid grid-cols-2 '>
-                                <div className='col-start-1'>{getButton("bg-blue", view, () => navigate(`/Projects/${item._id}`))}</div>
+                                <div className='col-start-1'>{getButton("text-blue", view, () => navigate(`/Projects/${item._id}`))}</div>
                                
-                                <div className='col-start-2'>{getButton("bg-red", bin,()=>deleteProject(item._id))}</div>
+                                <div className='col-start-2'>{getButton("text-red", bin,()=>deleteProject(item._id))}</div>
                             </div></td>
 
 
